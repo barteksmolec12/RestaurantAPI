3ba1a25ac30938e67cf13e4d921993f07f3da2b4
barteksmolec12 <barteksmolec@o2.pl>
2022-05-03 10:17:26 +0200

    Created RestaurantController"

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+	[Route("api/restaurant")]
+	public class RestaurantController : ControllerBase
+	{
+		private readonly RestaurantDbContext _dbContext;
+		public RestaurantController(RestaurantDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+		[HttpGet]
+		public ActionResult<IEnumerable<Restaurant>> GetAll()
+		{
+			var restaurants = _dbContext.Restaurant.ToList();
+			return Ok(restaurants);
+		}
+		[Route("api/restaurant/{id}")]
+		[HttpGet("{id}")]
+		public ActionResult<Restaurant> Get([FromRoute]int id)
+		{
+			var restaurant = _dbContext.Restaurant.FirstOrDefault(r => r.Id == id);
+			if(restaurant is null)
+			{
+				return NotFound();
+			}
+			
+			return Ok(restaurant);
+		}
+	}
+}
