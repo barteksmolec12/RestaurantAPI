29724622c33d9dd73d8a9e100fce08fb9e24dc7e
barteksmolec12 <barteksmolec@o2.pl>
2022-05-03 10:12:41 +0200

    Changes in entities

--- Entities/Address.cs
+++ Entities/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace RestaurantAPI.Entities
 	public class Address
 	{
 		public int Id { get; set; }
+		[MaxLength(50)]
+		[Required]
 		public string City { get; set; }
+		[MaxLength(50)]
+		[Required]
 		public string Street { get; set; }
 		public string PostalCode { get; set; }
 		public virtual Restaurant Restaurant { get; set; }
