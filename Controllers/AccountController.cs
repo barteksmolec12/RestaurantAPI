986419ee78191ab28fd2101a2cf86d670f6e6ead
barteksmolec12 <barteksmolec@o2.pl>
2022-05-25 18:40:17 +0200

    Created AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{ 
+	[Route("api/account")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		[HttpPost("register")]
+		public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+		{
+
+		}
+	}
+}
