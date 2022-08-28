374ecc896d8e619593e6b4e9c3e3791f55cb3f88
barteksmolec12 <barteksmolec@o2.pl>
2022-08-28 10:26:54 +0200

    Returning static files

---
+++ Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+	[Route("file") ] //ten kontroler nie będzie służył do zwracania zasobów z bazy danych
+	[Authorize]
+	public class FileController:ControllerBase
+	{
+		public ActionResult GetFile([FromQuery] string fileName)
+		{
+			var rootPath = Directory.GetCurrentDirectory();
+
+			var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+
+			var fileExist = System.IO.File.Exists(filePath);
+
+			if (!fileExist)
+			{ 
+				return NotFound(); 
+			}
+			var contentProvider= new FileExtensionContentTypeProvider();
+			contentProvider.TryGetContentType(filePath,out string contentType);
+			var fileContents=System.IO.File.ReadAllBytes(filePath);
+
+			return File(fileContents, contentType, fileName);
+
+		}
+	}
+}
